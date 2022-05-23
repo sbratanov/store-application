b9b6b8455fa074fdb1b50680b864501170eefa82
StanislavBratanov <stanislav_bratanov@abv.bg>
2022-05-23 23:09:26 +0300

    Initial version of the application, including the first batch of ngModules.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'products',
+  loadChildren: () => import('./modules/products/products.module').then(m => m.ProductsModule)
+},
+{
+  path: 'settings',
+  loadChildren: () => import('./modules/settings/settings.module').then(m => m.SettingsModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
